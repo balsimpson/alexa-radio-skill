bb9a4df137c19fca485a57e0346a743a7f1ea1a9
balsipson <thirdi@gmail.com>
2022-12-16 20:26:19 +0530

    updated radio api

--- server/api/radio.ts
+++ server/api/radio.ts
@@ -1,5 +1,6 @@
 export default defineEventHandler( async (event) => {
   // const config = useRuntimeConfig()
+  console.log("event", event)
   return {
     name: "Blues",
     url: "https://stream-39.zeno.fm/agtp9c146qzuv?zs=azWAiz9SQTCC2pjV4LgLUg"
