3d4b4e4a2ddca1182e830c4d6283c977552e3007
balsipson <thirdi@gmail.com>
2022-12-20 00:47:08 +0530

    trnamed to radio.ts

--- server/api/radio.ts
+++ server/api/radio.ts
@@ -1,10 +1,10 @@
 export default defineEventHandler( async (event) => {
   // const config = useRuntimeConfig()
-  const body = await readBody(event)
-  const query = getQuery(event)
+  // const body = await readBody(event)
+  // const query = getQuery(event)
   // console.log("event", event.context, body)
-  console.log("body", body)
-  console.log("query", query)
+  // console.log("body", body)
+  // console.log("query", query)
   return {
     name: "Blues",
     url: "https://stream-39.zeno.fm/agtp9c146qzuv?zs=azWAiz9SQTCC2pjV4LgLUg"
