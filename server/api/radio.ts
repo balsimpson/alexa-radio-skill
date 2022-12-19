44e3dee582ff61e6a9fb28c234f5d7c7743a9c60
balsipson <thirdi@gmail.com>
2022-12-20 00:03:08 +0530

    Added event.re to post request log

--- server/api/radio.ts
+++ server/api/radio.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler( async (event) => {
   // const config = useRuntimeConfig()
-  console.log("event", event.context)
+  console.log("event", event.context, event.req)
   return {
     name: "Blues",
     url: "https://stream-39.zeno.fm/agtp9c146qzuv?zs=azWAiz9SQTCC2pjV4LgLUg"
