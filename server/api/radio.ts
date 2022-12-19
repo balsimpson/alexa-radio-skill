5e1bca9fc3317f121d1b89ccd20fb3973608e3e6
balsipson <thirdi@gmail.com>
2022-12-20 00:34:22 +0530

    updated radio.ts

--- server/api/radio.ts
+++ server/api/radio.ts
@@ -2,7 +2,7 @@ export default defineEventHandler( async (event) => {
   // const config = useRuntimeConfig()
   const body = await readBody(event)
   const query = getQuery(event)
-  console.log("event", event.context, body)
+  // console.log("event", event.context, body)
   console.log("body", body)
   console.log("query", query)
   return {
