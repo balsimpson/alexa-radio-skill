444d5060f62684df767ea4c8bb99e728ba0143a5
balsipson <thirdi@gmail.com>
2022-12-29 15:58:21 +0530

    added vue-toastification to nuxt config build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,9 @@ export default defineNuxtConfig({
       },
     css: ["~/assets/css/styles.css"],
     modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
+    build : {
+      transpile: ['vue-toastification']
+   },
     publicRuntimeConfig: {
       FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
       FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID
